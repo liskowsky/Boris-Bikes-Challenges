471cade75f9dfae740bf0f567d70543b5a722b5f
Sara <sarah.alalaya@gmail.com>
2016-07-05 19:55:38 +0300

    Challenge 6

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,2 +1,2 @@
-class DockingStation
+class DockingStation 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,9 @@
 #DockingStation_spec.rb
-require "docking_station"
+require "docking_station" 
+require "spec_helper.rb"
 describe DockingStation do
-
-
+  it "Docking station responds to release bike" do
+    expect(DockingStation.respond_to release_bike)
+  end
 
 end
