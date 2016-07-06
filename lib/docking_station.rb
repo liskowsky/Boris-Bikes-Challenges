805ff78f098bfea538da9068788cfe9d8701fc18
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-06 15:31:18 +0200

    Exercise 8 back to the unit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,5 @@
 class Docking_station
+  def release_bike 
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,5 +2,7 @@ require 'docking_station'
 
 
 describe Docking_station do
-
+#it 'station release_bike' do
+#   expect(Docking_station).to respond_to(:release_bike)
+  it {is_expected.to respond_to :release_bike} 
 end
