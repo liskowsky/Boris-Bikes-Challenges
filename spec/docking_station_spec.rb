6134212e59b62b99038d85167723717517208e4c
Arunas Skirius <arukomp@gmail.com>
2016-07-07 15:28:10 +0100

    Step 11 - Using Instance Variables completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,13 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it 'shows the docked bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
